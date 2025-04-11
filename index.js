cb7e6392afcf96527054f68a1b4cd77f93bb80b6
FAGNER GOMES DA SILVA <a92269832@rn.senac.local>
2025-04-11 20:16:59 -0300

    Primeiro commit

--- index.js
+++ index.js
@@ -7,8 +7,37 @@ class AlunoModel{
         const resultado = await client.query(consulta, dados);
         return resultado.rows;
     }
-    
-
+    static async listarTodos(){
+        const consulta = `select * from aluno`
+        const resultado = await client.query(consulta);
+        return resultado.rows;
+    }
+    static async listarPorMatricula(email){
+        const dados = [email]
+        const consulta = `select * from aluno where email = $1`
+        const resultado = await client.query(consulta, dados);
+        return resultado.rows;
+    }
+    static async atualizarAluno(nome, email, matricula, telefone, cod_turma){
+        const dados = [nome, email, matricula, telefone, cod_turma]
+        const consulta = `update from aluno set nome = $1, matricula = $3, telefone = $4, cod_turma = $5 where email = $2 returning *;`
+        const resultado = await client.query(consulta, dados);
+        return resultado.rows;
+    }
+    static async deletarAluno(email){
+        const dados = [email]
+        const consulta = `delete from aluno where email = $1`
+        await client.query(consulta, dados);
+    }
+    static async deletarTodos(){
+        const consulta = `delete from aluno`
+        await client.query(consulta);
+    }
+    static async totalAlunos(){
+        const consulta = `select count(email) as total from aluno `
+        const resultado = await client.query(consulta);
+        return resultado.rows;
+    }
 }
 
 export default AlunoModel;
