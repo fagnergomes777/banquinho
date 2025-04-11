b649220bd8b6f7c9aba65f40b6b4f561cc241033
FAGNER GOMES DA SILVA <a92269832@rn.senac.local>
2025-04-10 21:17:35 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import client from "../../../config/database.js";
+class AlunoModel{
+    static async criar(nome, email, matricula, telefone, cod_turma){
+        const dados = [nome, email, matricula, telefone, cod_turma]
+        const consulta = `insert into aluno(nome, email, matricula, telefone, cod_turma)
+        values($1, $2, $3, $4, $5) returning *;`
+        const resultado = await client.query(consulta, dados);
+        return resultado.rows;
+    }
+
+}
+
+AlunoModel.criar(`Fagner`, `fagner@gmail.com`, `a0000`, `84999999999`,161)
